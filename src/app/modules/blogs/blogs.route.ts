bed7b882a450d56b0c636af662a814777dbfd9c1
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-19 23:04:33 +0600

    [add + mod] : update blog route added in blogs.route.ts

--- src/app/modules/blogs/blogs.route.ts
+++ src/app/modules/blogs/blogs.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
-import { blogCreationSchema } from "./blogs.validation";
+import { blogCreationSchema, blogUpdateSchema } from "./blogs.validation";
 import { BlogController } from "./blogs.controller";
 
 const BlogRouter: Router = Router();
 
 BlogRouter.post('/',validateRequest(blogCreationSchema), BlogController.createBlog);
+
+BlogRouter.patch('/:id',validateRequest(blogUpdateSchema), BlogController.updateBlog);
 
 export default BlogRouter;
