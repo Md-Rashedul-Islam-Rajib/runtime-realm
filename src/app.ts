1edc66b46edeb68b36ad8d96fea61da2b34319f0
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-19 14:55:50 +0600

    [add] : app.ts added

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import router from './app/routes';
+import { handleErrors } from './app/utilities/globalErrorHandler';
+import cookieParser from 'cookie-parser';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', router);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from Runtime Realm');
+});
+
+app.use(handleErrors);
+export default app;
