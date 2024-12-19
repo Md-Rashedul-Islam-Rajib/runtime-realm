db1af11565cfcf3b46cc2ac897344a3fe7018fdb
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-19 14:57:17 +0600

    [add] : server.ts added

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/', router);
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello from Runtime Realm');
 });
 
