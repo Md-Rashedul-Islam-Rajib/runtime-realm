47601b022a965ee5fc5dd3ca864c97dedb6599ef
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-19 14:58:57 +0600

    [add] : env configuration added

---
+++ src/app/config/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.join(process.cwd(), '.env'),
+});
+
+export default {
+  port: process.env.PORT as string,
+  NODE_ENV: process.env.NODE_ENV as string,
+  db_url: process.env.DB_URL as string,
+  default_password: process.env.DEFAULT_PASSWORD as string,
+  bcrypt_salt_rounds: process.env.SALT_ROUND as string,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET as string,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as string,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string,
+};
