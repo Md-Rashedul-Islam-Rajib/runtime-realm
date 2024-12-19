2dffa4ba0e95d189ffb6b6a9fe498f0c9f9946a4
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-19 23:11:25 +0600

    [add + mod] : delete blog service added in blogs.service.ts

--- src/app/modules/blogs/blogs.service.ts
+++ src/app/modules/blogs/blogs.service.ts
@@ -1,20 +1,31 @@
-import { StatusFullError } from "../../class/statusFullError";
-import { BlogModel } from "./blogs.model";
-import { TBlogs } from "./blogs.types";
+import { StatusFullError } from '../../class/statusFullError';
+import { BlogModel } from './blogs.model';
+import { TBlogs } from './blogs.types';
 
 export class BlogServices {
     static async createBlog(paylaod: TBlogs) {
         const result = await BlogModel.create(paylaod);
         return result;
-    };
+    }
 
-    static async updateBlog(id:string,paylaod: Partial<TBlogs>) {
+    static async updateBlog(id: string, paylaod: Partial<TBlogs>) {
         const isBlogExists = await BlogModel.findById(id);
-        if(!isBlogExists) {
-            throw new StatusFullError("NotFoundError","Blog not found",true,404);
+        if (!isBlogExists) {
+            throw new StatusFullError('NotFoundError', 'Blog not found', true, 404);
         }
-
-        const result = await BlogModel.findByIdAndUpdate(id, paylaod, { new: true, runValidators: true });
+        const result = await BlogModel.findByIdAndUpdate(id, paylaod, {
+            new: true,
+            runValidators: true,
+        });
         return result;
+    }
+
+    static async deleteBlog(id: string) {
+        const isBlogExists = await BlogModel.findById(id);
+        if (!isBlogExists) {
+            throw new StatusFullError('NotFoundError', 'Blog not found', true, 404);
+        };
+            const result = await BlogModel.findByIdAndDelete(id,{new:true});
+            return result;
     }
 }
