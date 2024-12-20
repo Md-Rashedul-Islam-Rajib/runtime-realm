f5121b8dc94dac3c990282fe8f540df2b4fa3cc2
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-20 14:22:50 +0600

    [add + mod] :  admin login route added in admin.route.ts

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -3,6 +3,8 @@ import { AdminControllers } from "./admin.controller";
 
 const AdminRouter: Router = Router();
 
-AdminRouter.post('/register',AdminControllers.registerAdmin);
+AdminRouter.post('/register', AdminControllers.registerAdmin);
+
+AdminRouter.post('/login',AdminControllers.loginAdmin);
 
 export default AdminRouter;
