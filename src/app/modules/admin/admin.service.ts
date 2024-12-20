270531cdb668271ac90cacb8684c1f1cd486804b
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-20 14:18:29 +0600

    [add + mod] :  admin login service added in admin.service.ts

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,14 +1,56 @@
 import { StatusFullError } from '../../class/statusFullError';
-import { AdminModel } from './admin.model';
-import { TAdmin } from './admin.types';
+import config from '../../config';
+import { TLoginUser } from '../auth/auth.types';
+import { createToken, preValidatingUser } from '../auth/auth.utilities';
+import { UserModel } from '../user/user.model';
+import { TUser } from '../user/user.types';
+
 
 export class AdminServices {
-  static async registerAdmin(payload: TAdmin) {
-      const adminCount = await AdminModel.countDocuments();
+  static async registerAdmin(payload: TUser) {
+      const adminCount = await UserModel.findOne({role: 'admin'});
       if (adminCount) {
           throw new StatusFullError('AuthenticationError',"admin already registered",false,403);
       }
-    const result = await AdminModel.create(payload);
+    const result = await UserModel.create(payload);
     return result;
-  }
-}
+    }
+    
+    
+    static async loginUser(payload: TLoginUser) {
+            const admin = await preValidatingUser(payload.email);
+    
+            const isPasswordCorrect = await UserModel.isPasswordMatched(
+                payload?.password,
+                admin?.password,
+            );
+            
+            if (!isPasswordCorrect) {
+                throw new StatusFullError("AuthenticationError","Password is incorrect",true, 401);
+            }
+    
+            const jwtPayload = {
+                email: admin.email,
+                role: admin.role,
+            };
+    
+            const accessToken = createToken(
+                jwtPayload,
+                config.jwt_access_secret,
+                config.jwt_access_expires_in
+            );
+    
+            const refreshToken = createToken(
+              jwtPayload,
+              config.jwt_refresh_secret,
+              config.jwt_refresh_expires_in,
+            );
+    
+            return {
+              accessToken,
+              refreshToken,
+            };
+    
+        }
+
+    }
--- src/app/modules/admin/admin.types.ts
+++
@@ -1,5 +0,0 @@
-export type TAdmin = {
-    name: "runtime-admin";
-    email: "runtime-admin@realm.com";
-    password: "runtime_realm"
-}
